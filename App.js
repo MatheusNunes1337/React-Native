a0a027f6c598d4999a3e1e59d3b6918424d0e54c
Matheus <matheusyagami147@gmail.com>
2021-10-24 19:12:44 -0300

    feat(app): add home screen to the navigation

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import SignIn from './src/screens/SignIn'
+import SignUp from './src/screens/SignUp'
+import Home from './src/screens/Home'
 import {View, Text} from 'react-native'
 
 const Stack = createNativeStackNavigator();
@@ -11,6 +13,8 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Sign In" component={SignIn} />
+        <Stack.Screen name="Sign Up" component={SignUp} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
