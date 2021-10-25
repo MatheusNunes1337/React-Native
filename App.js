1778ee35b7b3781da4cf9fb076ea6f426760b58a
Matheus <matheusyagami147@gmail.com>
2021-10-24 21:32:52 -0300

    feat(app.js): change the status bar color

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import SignIn from './src/screens/SignIn'
 import SignUp from './src/screens/SignUp'
 import Home from './src/screens/Home'
-import {View, Text} from 'react-native'
+import { StatusBar } from 'react-native'
+import { primary } from './src/assets/colors'
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor={primary} />
       <Stack.Navigator initialRouteName="SignIn">
         <Stack.Screen name="SignIn" component={SignIn} />
         <Stack.Screen name="Home" component={Home} />
