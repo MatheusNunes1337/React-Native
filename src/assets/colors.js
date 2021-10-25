6a3d8769876611a8b2ff059d7bd168895044843e
Matheus <matheusyagami147@gmail.com>
2021-10-24 21:30:05 -0300

    feat(SignIn): finish the style in the page

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -1,4 +1,5 @@
 export const primary = "#3799CE";
-export const dark = "464646";
-export const primaryDark = "#1b305a";
-export const white = "#1b305a";
+export const dark = "#464646";
+export const darkBlue = "#1b305a";
+export const white = "#fff";
+export const gray = "gray"
--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,14 +1,44 @@
 import React from 'react'
-import {View, StyleSheet, Text, Image} from 'react-native'
+import {View, StyleSheet, Text, Image, TextInput, Button} from 'react-native'
+import MeuButton from '../components/MeuButton'
+import { white, dark, darkBlue, primary, gray } from '../assets/colors'
+
 
 const SignIn = (props) => {
+
+    function recuperarSenha() {
+        alert('abrir modal de recuperar senha!')
+    }
+
+    function entrar() {
+        alert('entrei!')
+    }
+
+    function cadastrar() {
+        alert('vai para a screen sign up')
+    }
+
     return (
         <View style={styles.container}>   
             <View style={styles.divSuperior}>
-               <Image source={require('../assets/images/logo.png')} /> 
+               <Image style={styles.logo} 
+               accessibilityLabel="logo do app"
+               source={require('../assets/images/logo.png')} />
+               <TextInput style={styles.input}/>
+               <TextInput style={styles.input}/>
+               <Text style={styles.textEsqueceuSenha} onPress={recuperarSenha}>Esqueceu sua senha?</Text>
+               <MeuButton texto="Entrar" onClick={entrar}/>
             </View>
             <View style={styles.divInferior}>
-                
+                <View style={styles.divOuHr}>
+                    <View style={styles.divHr} />
+                    <Text style={styles.textOu}>OU</Text>
+                    <View style={styles.divHr} />
+                </View>
+                <View style={styles.divCadastrarSe}>
+                    <Text style={styles.textNormal}>Não tem uma conta?</Text>
+                    <Text style={styles.textCadastrarSe} onPress={cadastrar}>Cadastre-se</Text>
+                </View>
             </View>
         </View>   
     )
@@ -26,13 +56,74 @@ const styles = StyleSheet.create({
     divSuperior: {
         flex: 5,
         alignItems: 'center',
-        backgroundColor: 'red'
     },
 
     divInferior: {
         flex: 1,
         alignItems: 'center',
         marginTop: 25,
-        backgroundColor: 'blue'
     },
+
+    logo: {
+        width: 150,
+        height: 150,
+        margin: 15
+    },
+    
+    input: {
+        width: '95%',
+        height: 50,
+        borderBottomColor: gray,
+        borderBottomWidth: 2,
+        fontSize: 16,
+        paddingLeft: 2,
+        paddingBottom: 1
+    },
+
+    textEsqueceuSenha: {
+        fontSize: 18,
+        color: dark,
+        alignSelf: 'flex-end',
+        marginTop: 14,
+        marginBottom: 14
+    },
+    divOuHr: {
+        width: '100%',
+        height: 25,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    divHr: {
+        width: '30%',
+        height: 1,
+        borderBottomColor: gray,
+        borderBottomWidth: 2
+    },
+
+    textOu: {
+        marginLeft: 20,
+        marginRight: 20,
+        fontSize: 20,
+        color: gray
+    },
+
+    divCadastrarSe: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20
+    },
+
+    textNormal: {
+        fontSize: 18,
+    },
+
+    textCadastrarSe: {
+        fontSize: 16,
+        color: primary,
+        marginLeft: 8
+    }
 })
