4bacee1617addf173c66fec6cf7962f8fbdb3835
Matheus <matheusyagami147@gmail.com>
2021-10-30 14:42:28 -0300

    feat(SignUp): add TextInput component style to SignUp screen

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import {Body} from './styles.js';
+import {Body, TextInput} from './styles.js';
+import MeuButton from '../../components/MeuButton';
 
 const SignUp = () => {
-  return <Body />;
+  const type = 'teacher';
+  const cadastrar = () => {
+    alert('cadastrei');
+  };
+
+  return (
+    <Body>
+      <TextInput />
+      <TextInput />
+      <TextInput />
+      <TextInput />
+      {type === 'teacher' ? <TextInput /> : ''}
+      <MeuButton texto="Cadastrar" onClick={cadastrar} />
+    </Body>
+  );
 };
 
 export default SignUp;
