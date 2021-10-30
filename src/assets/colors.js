d2eabdb579d4f8b733faaff64bb94524acaabbc3
Matheus <matheusyagami147@gmail.com>
2021-10-30 14:18:27 -0300

    feat: add ice color to colors.js in assets

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -1,5 +1,6 @@
-export const primary = "#3799CE";
-export const dark = "#464646";
-export const darkBlue = "#1b305a";
-export const white = "#fff";
-export const gray = "gray"
+export const primary = '#3799CE';
+export const dark = '#464646';
+export const darkBlue = '#1b305a';
+export const white = '#fff';
+export const gray = 'gray';
+export const ice = '#ebebf7';
