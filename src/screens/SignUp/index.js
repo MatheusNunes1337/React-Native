12c1d5c4216377d3370408178d56ea18407dc0a4
Matheus <matheusyagami147@gmail.com>
2021-10-30 15:21:51 -0300

    feat(SignUp): implement the cadastrar method

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,16 +1,54 @@
 import React, {useState} from 'react';
+import {Alert} from 'react-native';
 import {Body, TextInput} from './styles.js';
 import MeuButton from '../../components/MeuButton';
+import auth from '@react-native-firebase/auth';
+import {CommonActions} from '@react-navigation/routers';
 
-const SignUp = () => {
+const SignUp = ({navigation}) => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [type, setType] = useState('student');
   const [area, setArea] = useState('');
 
-  const cadastrar = () => {
-    alert('cadastrei');
+  const cadastrar = async () => {
+    try {
+      if (
+        username.length === 0 ||
+        email.length === 0 ||
+        password.length === 0 ||
+        type.length === 0
+      ) {
+        throw new Error('Campos de Email e senha não pode estar vazios');
+      }
+      await auth().createUserWithEmailAndPassword(email, password);
+      Alert.alert('Informação', 'Usuário cadastrado com sucesso');
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 0,
+          routes: [{name: 'Home'}],
+        }),
+      );
+    } catch (err) {
+      switch (err.code) {
+        case 'auth/email-already-in-use':
+          Alert.alert('Erro', 'Esse email já está em uso');
+          break;
+        case 'auth/operation-not-allowed':
+          Alert.alert('Erro', 'Algo de errado ocorreu ao cadastrar o usuário');
+          break;
+        case 'auth/invalid-email':
+          Alert.alert('Erro', 'Email inválido');
+          break;
+        case 'auth/weak-password':
+          Alert.alert('Erro', 'A senha informada é fraca. Tente outra');
+          break;
+        default:
+          Alert.alert('Erro', err.message);
+          break;
+      }
+    }
   };
 
   return (
