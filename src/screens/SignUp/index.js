f1d8647b1ecb4776aaf4bf9120c66ab0a09429a3
Matheus <matheusyagami147@gmail.com>
2021-10-30 14:58:38 -0300

    feat(SignUp): add atributes to TextInput components

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Body, TextInput} from './styles.js';
 import MeuButton from '../../components/MeuButton';
 
 const SignUp = () => {
-  const type = 'teacher';
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [type, setType] = useState('student');
+  const [area, setArea] = useState('');
+
   const cadastrar = () => {
     alert('cadastrei');
   };
 
   return (
     <Body>
-      <TextInput />
-      <TextInput />
-      <TextInput />
-      <TextInput />
-      {type === 'teacher' ? <TextInput /> : ''}
+      <TextInput
+        placeholder="Nome de usuário"
+        keyboardType="default"
+        returnKeyType="next"
+        onChangeText={e => setUsername(e)}
+      />
+      <TextInput
+        placeholder="Email"
+        keyboardType="email-address"
+        returnKeyType="next"
+        onChangeText={e => setEmail(e)}
+      />
+      <TextInput
+        secureTextEntry={true}
+        placeholder="Senha"
+        keyboardType="default"
+        returnKeyType="next"
+        onChangeText={p => setPassword(p)}
+      />
+      <TextInput
+        placeholder="Tipo"
+        keyboardType="default"
+        returnKeyType="next"
+        onChangeText={e => setType(e)}
+      />
+      {type === 'teacher' ? (
+        <TextInput
+          placeholder="Área"
+          keyboardType="default"
+          returnKeyType="go"
+          onChangeText={e => setArea(e)}
+        />
+      ) : (
+        false
+      )}
       <MeuButton texto="Cadastrar" onClick={cadastrar} />
     </Body>
   );
