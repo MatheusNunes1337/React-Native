d69966ff511a225ffb02a0b7b904bd27b2bb0a8d
Matheus <matheusyagami147@gmail.com>
2021-10-30 14:41:45 -0300

    feat(SignUp): create the style for TextInput component

--- src/screens/SignUp/styles.js
+++ src/screens/SignUp/styles.js
@@ -1,7 +1,19 @@
 import styled from 'styled-components/native';
-import {primary} from '../../assets/colors';
+import {primary, white, gray} from '../../assets/colors';
 
-export const Body = styled.ScrollView`
+export const Body = styled.View`
   flex: 1;
-  background-color: ${primary};
+  align-items: center;
+  padding: 10px 0px;
+`;
+
+export const TextInput = styled.TextInput`
+  width: 95%;
+  height: 50px;
+  border-bottom-color: ${gray};
+  border-bottom-width: 2px;
+  font-size: 16px;
+  margin-bottom: 12px;
+  padding-left: 2px;
+  padding-bottom: 1px;
 `;
