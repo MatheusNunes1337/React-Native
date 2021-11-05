a2afa58eaf5d86a434ba73cb45f8386fbe2f27b4
Matheus <matheusyagami147@gmail.com>
2021-11-04 21:16:15 -0300

    refactor(screens): recortado o método getUserCache de Home e colado em Preload

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import {View, Text, Alert} from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Home = () => {
-  const getUserCache = async () => {
-    try {
-      const user = await AsyncStorage.getItem('user');
-      console.log('user cache', user);
-      return user != null ? JSON.parse(user) : null;
-    } catch (err) {
-      Alert.alert('Erro', err.message);
-    }
-  };
-
-  getUserCache();
   return (
     <View>
       <Text>Home</Text>
--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Alert} from 'react-native';
 import {Container, Image} from './styles';
+import {CommonActions} from '@react-navigation/routers';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import auth from '@react-native-firebase/auth';
 
-const Preload = () => {
+const Preload = ({navigation}) => {
+  const getUserCache = async () => {
+    try {
+      const user = await AsyncStorage.getItem('user');
+      return user != null ? JSON.parse(user) : null;
+    } catch (err) {
+      Alert.alert('Erro', err.message);
+    }
+  };
+
+  const login = async () => {
+    try {
+      const user = await getUserCache();
+      if (!user) {
+        navigation.dispatch(
+          CommonActions.reset({
+            index: 0,
+            routes: [{name: 'SignIn'}],
+          }),
+        );
+      }
+      await auth().signInWithEmailAndPassword(user.email, user.password);
+      navigation.dispatch(
+        CommonActions.reset({
+          index: 0,
+          routes: [{name: 'Home'}],
+        }),
+      );
+    } catch (err) {
+      switch (err.code) {
+        case 'auth/user-not-found':
+          Alert.alert('Erro', 'Usuário não encontrado');
+          break;
+        case 'auth/wrong-password':
+          Alert.alert('Erro', 'Algo de errado ocorreu com a sua senha');
+          break;
+        case 'auth/invalid-email':
+          Alert.alert('Erro', 'Email inválido');
+          break;
+        case 'auth/user-disabled':
+          Alert.alert('Erro', 'Usuário desabilitado');
+          break;
+        default:
+          Alert.alert('Erro', err.message);
+          break;
+      }
+    }
+  };
+
+  useEffect(() => {
+    login();
+  }, []);
+
   return (
     <Container>
       <Image
