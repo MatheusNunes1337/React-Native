dc934944fdb1dfaded03d225ab5fae0e303a1099
Matheus <matheusyagami147@gmail.com>
2021-11-07 19:28:12 -0300

    feat: add ListItem component from React native elements in Home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,38 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, Alert, StyleSheet} from 'react-native';
 import LogoutButton from '../components/LogoutButton';
+import {ListItem, Button} from 'react-native-elements';
+import {primary} from '../assets/colors';
 
 const Home = ({navigation}) => {
+  const [list, setLIst] = useState([]);
+
   useEffect(() => {
     navigation.setOptions({
       title: 'Usuários',
       headerRight: () => <LogoutButton />,
     });
+    getUsers();
   }, []);
+
+  const routeUser = () => {
+    alert('oi');
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      {list.map((item, i) => (
+        <ListItem
+          key={i}
+          containerStyle={styles.list}
+          onPress={routeUser}
+          bottomDivider>
+          <ListItem.Content>
+            <ListItem.Title>{item.nome}</ListItem.Title>
+            <ListItem.Title>{item.email}</ListItem.Title>
+          </ListItem.Content>
+        </ListItem>
+      ))}
     </View>
   );
 };
@@ -23,5 +43,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+  },
+  list: {
+    width: 350,
+    height: 100,
+    marginTop: 25,
   },
 });
