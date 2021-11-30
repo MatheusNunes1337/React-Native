ad0829f039dac80ece1d018c9d93a82879f2f270
Matheus Nunes <matheusyagami147@gmail.com>
2021-11-30 15:35:02 -0300

    refactor: remove all configs from app.js file then import Providers

--- App.js
+++ App.js
@@ -1,70 +1,8 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import SignIn from './src/screens/SignIn';
-import SignUp from './src/screens/SignUp';
-import ForgotPassword from './src/screens/ForgotPassword';
-import Home from './src/screens/Home';
-import Preload from './src/screens/Preload';
-import {StatusBar} from 'react-native';
-import {primary, white} from './src/assets/colors';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import User from './src/screens/User';
 
-const Stack = createNativeStackNavigator();
+import Providers from './src/navigation';
 
-function App() {
-  return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <StatusBar backgroundColor={primary} />
-        <Stack.Navigator initialRouteName="Preload">
-          <Stack.Screen
-            name="Preload"
-            component={Preload}
-            options={preloadStyle}
-          />
-          <Stack.Screen
-            name="SignIn"
-            component={SignIn}
-            options={signInStyle}
-          />
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="User" component={User} options={userStyle} />
-          <Stack.Screen
-            name="SignUp"
-            component={SignUp}
-            options={signUpStyle}
-          />
-          <Stack.Screen
-            name="ForgotPassword"
-            component={ForgotPassword}
-            options={forgotPasswordStyle}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
-  );
+export default function App() {
+  return <Providers />;
 }
-
-export default App;
-
-const preloadStyle = {
-  headerShown: false,
-};
-
-const signInStyle = {
-  title: 'Bem vindo',
-};
-
-const signUpStyle = {
-  title: 'Cadastrar',
-};
-
-const userStyle = {
-  title: 'Usuário',
-};
-
-const forgotPasswordStyle = {
-  title: 'Recuperar senha',
-};
