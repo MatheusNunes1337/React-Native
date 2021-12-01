f68398e814e27714b606dd06259a5ea9e776e9f5
Matheus Nunes <matheusyagami147@gmail.com>
2021-11-30 21:56:09 -0300

    feat: add GroupProvider to navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,13 +3,16 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {AuthUserProvider} from '../context/AuthUserProvider';
 import {ApiProvider} from '../context/ApiProvider';
 import Routes from './Routes';
+import {GroupProvider} from '../context/GroupProvider';
 
 export default function Providers() {
   return (
     <SafeAreaProvider>
       <AuthUserProvider>
         <ApiProvider>
-          <Routes />
+          <GroupProvider>
+            <Routes />
+          </GroupProvider>
         </ApiProvider>
       </AuthUserProvider>
     </SafeAreaProvider>
