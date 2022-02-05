633717ab957c4d5125b0e8248b2a066da95d8e3c
Matheus Nunes <matheusyagami147@gmail.com>
2022-02-05 15:00:47 -0300

    feat: add report provider to app providers

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -4,15 +4,18 @@ import {AuthUserProvider} from '../context/AuthUserProvider';
 import {ApiProvider} from '../context/ApiProvider';
 import Routes from './Routes';
 import {GroupProvider} from '../context/GroupProvider';
+import {ReportProvider} from '../context/ReportProvider';
 
 export default function Providers() {
   return (
     <SafeAreaProvider>
       <AuthUserProvider>
         <ApiProvider>
-          <GroupProvider>
-            <Routes />
-          </GroupProvider>
+          <ReportProvider>
+            <GroupProvider>
+              <Routes />
+            </GroupProvider>
+          </ReportProvider>
         </ApiProvider>
       </AuthUserProvider>
     </SafeAreaProvider>
