b23037133e4c69f9b7c3edb6cd4115f90b909be0
Matheus Nunes <matheusyagami147@gmail.com>
2022-02-14 21:21:10 -0300

    feat: add signOut method from LogoutButton to AuthUserProvider

--- src/context/AuthUserProvider.js
+++ src/context/AuthUserProvider.js
@@ -1,14 +1,28 @@
 import React, {useState, createContext} from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import auth from '@react-native-firebase/auth';
+import {Alert} from 'react-native';
 
 export const AuthUserContext = createContext({});
 
 export const AuthUserProvider = ({children}) => {
   const [user, setUser] = useState(null);
+
+  const signOut = async () => {
+    try {
+      await AsyncStorage.removeItem('user');
+      auth().signOut();
+    } catch (err) {
+      Alert.alert('Erro', err.message);
+    }
+  };
+
   return (
     <AuthUserContext.Provider
       value={{
         user,
         setUser,
+        signOut,
       }}>
       {children}
     </AuthUserContext.Provider>
